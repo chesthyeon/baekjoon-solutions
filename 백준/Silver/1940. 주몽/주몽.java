5a352d56c10f5274b0eafd384cf7f8f58aa7d3cd
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2024-08-14 21:13:20 +0900

    [Silver IV] Title: 주몽, Time: 168 ms, Memory: 16752 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,28 +1,36 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        
-        int[] materials = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        int[] materials = new int[N];
+        for (int i = 0; i < N; i++){
             materials[i] = Integer.parseInt(st.nextToken());
         }
-        
+
+        Arrays.sort(materials);
+
         int count = 0;
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (materials[i] + materials[j] == M) {
-                    count++;
-                    break;  // 이 재료 쌍을 찾았으므로 내부 루프를 종료
-                }
+        int left = 0;
+        int right = N - 1;
+
+        while (left < right){
+            int sum = materials[left] + materials[right];
+            if (sum == M){
+                count++;
+                left++;
+                right--;
             }
+            else if (sum < M){
+                left++;
+            }
+            else right--;
         }
-        
-        System.out.println(count);
+        System.out.print(count);
     }
 }
