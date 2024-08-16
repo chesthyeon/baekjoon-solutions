b7080a3f80a0fb3effa97db810bf3d39f9d789c7
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2024-08-16 16:37:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 102.55 ms, Memory: 95.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> participantMap = new HashMap<>();
+        for (String name :
+                participant) {
+            participantMap.put(name, participantMap.getOrDefault(name,0) + 1);
+        }
+        for (String name :
+                completion) {
+            participantMap.put(name, participantMap.get(name) - 1);
+        }
+        for (Map.Entry<String, Integer> entry :
+                participantMap.entrySet()) {
+            if (entry.getValue() != 0){
+                answer = entry.getKey();
+            }
+        }
+        return answer;
+    }
+}
