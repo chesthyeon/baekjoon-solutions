bfdcb773075bc2d7d0b4287ec21357270cf954c7
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2024-08-21 17:43:59 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -3,26 +3,23 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        //계단의 개수
         int n = Integer.parseInt(br.readLine());
-        int[] stairs = new int[n+1];
-        int[] dp = new int[n+1];
-
-        for (int i = 1; i <= n; i++) {
+        //계단 값 입력
+        int[] stairs = new int[n + 1];
+        for (int i = 1; i <= n; i++){
             stairs[i] = Integer.parseInt(br.readLine());
         }
-
-        // 초기값 설정
+        //dp 생성 및 초기화.
+        int[] dp = new int[n + 1];
         dp[1] = stairs[1];
         if (n >= 2) {
             dp[2] = stairs[1] + stairs[2];
         }
 
-        // 동적 프로그래밍
-        for (int i = 3; i <= n; i++) {
+        for (int i = 3; i <= n; i++){
             dp[i] = Math.max(dp[i-2] + stairs[i], dp[i-3] + stairs[i-1] + stairs[i]);
         }
-
         System.out.println(dp[n]);
     }
 }
