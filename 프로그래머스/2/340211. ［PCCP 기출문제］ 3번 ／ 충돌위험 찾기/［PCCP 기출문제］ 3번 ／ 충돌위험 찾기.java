93816b2a76bb9810d70d11ee64fd7e365f074b89
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2024-09-18 22:19:18 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 287.99 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,112 +1,79 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
-    private class Robot {
-        private int r;
-        private int c;
-        private int nextStopover;
-
-        public Robot(int r, int c, int nextStopover) {
+    static class Robot {
+        int r, c, nextStop;
+        Robot(int r, int c) {
             this.r = r;
             this.c = c;
-            this.nextStopover = nextStopover;
+            this.nextStop = 1;
         }
-
-        public void moveToTarget(int targetR, int targetC) {
-            if (r < targetR) {
-                r++;  
-                return;
-            } else if (r > targetR) {
-                r--; 
-                return;
-            }
-
-            if (c < targetC) {
-                c++; 
-            } else if (c > targetC) {
-                c--;  
+        
+        void move(int targetR, int targetC) {
+            if (r != targetR) {
+                r += Integer.compare(targetR, r);
+            } else if (c != targetC) {
+                c += Integer.compare(targetC, c);
             }
         }
-
-        public boolean hasArrived(int targetR, int targetC) {
+        
+        boolean hasArrived(int targetR, int targetC) {
             return r == targetR && c == targetC;
         }
-
-        public void stopoverPlus() {
-            nextStopover++;
-        }
     }
-
+    
     public int solution(int[][] points, int[][] routes) {
-        int routeStep = routes[0].length;
+        List<Robot> robots = initializeRobots(points, routes);
+        int collisionCount = countInitialCollisions(routes);
+        
+        while (!allRobotsFinished(robots, routes[0].length)) {
+            Map<String, Integer> positions = new HashMap<>();
+            moveRobots(robots, points, routes, positions);
+            collisionCount += countCollisions(positions);
+        }
+        
+        return collisionCount;
+    }
+    
+    private List<Robot> initializeRobots(int[][] points, int[][] routes) {
         List<Robot> robots = new ArrayList<>();
-        Map<Integer, Integer> startCollisions = new HashMap<>();
-
         for (int[] route : routes) {
             int startPoint = route[0] - 1;
-            int r = points[startPoint][0];
-            int c = points[startPoint][1];
-            robots.add(new Robot(r, c, 1));
-            startCollisions.merge(route[0], 1, Integer::sum);
+            robots.add(new Robot(points[startPoint][0], points[startPoint][1]));
         }
-
-        int result = 0;
-
-        for (Integer value : startCollisions.values()) {
-            if (value > 1) {
-                result++;
-            }
+        return robots;
+    }
+    
+    private int countInitialCollisions(int[][] routes) {
+        Map<Integer, Integer> startPositions = new HashMap<>();
+        for (int[] route : routes) {
+            startPositions.merge(route[0], 1, Integer::sum);
         }
-
-        while (!robots.isEmpty()) {
-            Map<String, Integer> collisionMap = new HashMap<>();
-            boolean[] arrived = new boolean[robots.size()];
-
-            for (int i = 0; i < robots.size(); i++) {
-                Robot robot = robots.get(i);
-                int nextStopover = robot.nextStopover;
-
-                if (nextStopover >= routeStep) {
-                    arrived[i] = true;
-                    continue;
+        return (int) startPositions.values().stream().filter(count -> count > 1).count();
+    }
+    
+    private boolean allRobotsFinished(List<Robot> robots, int routeLength) {
+        return robots.stream().allMatch(robot -> robot.nextStop >= routeLength);
+    }
+    
+    private void moveRobots(List<Robot> robots, int[][] points, int[][] routes, Map<String, Integer> positions) {
+        for (int i = 0; i < robots.size(); i++) {
+            Robot robot = robots.get(i);
+            if (robot.nextStop < routes[i].length) {
+                int[] targetPoint = points[routes[i][robot.nextStop] - 1];
+                robot.move(targetPoint[0], targetPoint[1]);
+                
+                String posKey = robot.r + "," + robot.c;
+                positions.merge(posKey, 1, Integer::sum);
+                
+                if (robot.hasArrived(targetPoint[0], targetPoint[1])) {
+                    robot.nextStop++;
                 }
-
-                int pointIndex = routes[i][nextStopover] - 1;
-                int targetR = points[pointIndex][0];
-                int targetC = points[pointIndex][1];
-
-                robot.moveToTarget(targetR, targetC);
-
-                String posKey = robot.r + " " + robot.c;
-                collisionMap.merge(posKey, 1, Integer::sum);
-
-                if (robot.hasArrived(targetR, targetC)) {
-                    robot.stopoverPlus();
-                }
-            }
-
-            for (Integer value : collisionMap.values()) {
-                if (value > 1) {
-                    result++;
-                }
-            }
-
-            boolean allArrived = true;
-            for (boolean arrivedStatus : arrived) {
-                if (!arrivedStatus) {
-                    allArrived = false;
-                    break;
-                }
-            }
-
-            if (allArrived) {
-                break;
             }
         }
-
-        return result;
+    }
+    
+    private int countCollisions(Map<String, Integer> positions) {
+        return (int) positions.values().stream().filter(count -> count > 1).count();
     }
 }
