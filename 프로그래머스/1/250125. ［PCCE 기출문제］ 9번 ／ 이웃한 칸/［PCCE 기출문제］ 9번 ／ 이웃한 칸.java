cde6c9e3573e0ff219f80420e8f0ed5e19c9d7e9
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2024-09-20 22:24:02 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.04 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -1,26 +1,17 @@
 class Solution {
     public int solution(String[][] board, int h, int w) {
-        int count = 0;
+        int answer = 0;
         int n = board.length;
-        
-        // 상하좌우 방향
-        int[] dh = {-1, 1, 0, 0};  // 행 방향 이동
-        int[] dw = {0, 0, -1, 1};  // 열 방향 이동
-        
-        // 주어진 칸의 색상
+        int[] dh = {-1, 1, 0, 0};
+        int[] dw = {0, 0, -1, 1};
         String color = board[h][w];
-        
-        // 상하좌우 칸 확인
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++){
             int nh = h + dh[i];
             int nw = w + dw[i];
-            
-            // 배열 범위 내에 있고, 색상이 같은 경우
-            if (nh >= 0 && nh < n && nw >= 0 && nw < n && board[nh][nw].equals(color)) {
-                count++;
+            if (nh >= 0 && nh < n && nw >= 0 && nw < n && color.equals(board[nh][nw])){
+                answer++;
             }
         }
-        
-        return count;
+        return answer;
     }
 }
