e54045ca498323f3e5746c91fd12f223c6488e2c
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2024-09-21 00:14:29 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 23.80 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -1,14 +1,18 @@
 class Solution {
     public long solution(int r1, int r2) {
-        long count = 0;
+        long answer = 0;
         
         for (int x = 0; x <= r2; x++) {
-            double y2 = Math.floor(Math.sqrt((long)r2 * r2 - (long)x * x));
-            double y1 = Math.ceil(Math.sqrt(Math.max(0, (long)r1 * r1 - (long)x * x)));
+            long y1 = (long) Math.ceil(Math.sqrt((long)r1 * r1 - (long)x * x));
+            long y2 = (long) Math.floor(Math.sqrt((long)r2 * r2 - (long)x * x));
             
-            count += (long)(y2 - y1 + 1);
+            if (x <= r1) {
+                answer += (y2 - y1 + 1);
+            } else {
+                answer += (y2 + 1);
+            }
         }
         
-        return count * 4 - (r2 - r1 + 1) * 4;
+        return answer * 4 - 4 * (r2 - r1 + 1);
     }
 }
