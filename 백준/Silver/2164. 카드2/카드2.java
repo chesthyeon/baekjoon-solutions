835a85cb3d404ed170e99cc271035c24cf3fbb06
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2024-09-27 20:35:50 +0900

    [Silver IV] Title: 카드2, Time: 152 ms, Memory: 45436 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,24 +1,20 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         Queue<Integer> queue = new LinkedList<>();
-
+        
         for (int i = 1; i <= N; i++){
-            queue.offer(i);
+            queue.add(i);
         }
-
+        
         while (queue.size() > 1){
             queue.poll();
             queue.offer(queue.poll());
         }
-        
-        System.out.print(queue.poll());
+        System.out.println(queue.poll());
     }
 }
