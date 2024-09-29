c95abfae97938203009039453cbb1246c24fba62
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2024-09-29 15:06:04 +0900

    [Silver II] Title: 쇠막대기, Time: 152 ms, Memory: 16396 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
+        int result = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(') {
+                stack.push('(');
+            } else {
+                stack.pop();
+                if (input.charAt(i-1) == '(') {
+                    // 레이저인 경우
+                    result += stack.size();
+                } else {
+                    // 쇠막대기의 끝인 경우
+                    result += 1;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
