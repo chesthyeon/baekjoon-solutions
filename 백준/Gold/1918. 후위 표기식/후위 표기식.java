15ba939f13ec5323c8252f77e7edafb8be35937b
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2024-09-30 20:02:52 +0900

    [Gold II] Title: 후위 표기식, Time: 96 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String infix = br.readLine();
+        System.out.println(infixToPostfix(infix));
+    }
+
+    public static String infixToPostfix(String infix) {
+        StringBuilder result = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : infix.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                result.append(c);
+            } else if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    result.append(stack.pop());
+                }
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                }
+            } else {
+                while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek())) {
+                    result.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            if (stack.peek() == '(') {
+                return "Invalid Expression";
+            }
+            result.append(stack.pop());
+        }
+
+        return result.toString();
+    }
+
+    public static int precedence(char ch) {
+        switch (ch) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+        }
+        return -1;
+    }
+}
