ae774a8cd7e841b78dd0394d1c127426750d4668
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2024-09-30 23:27:31 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2468 ms, Memory: 448640 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -4,38 +4,24 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
         int T = Integer.parseInt(br.readLine());
-        
-        for (int t = 0; t < T; t++) {
+        while (T-- > 0) {
             int k = Integer.parseInt(br.readLine());
-            TreeMap<Integer, Integer> queue = new TreeMap<>();
-            
-            for (int i = 0; i < k; i++) {
-                String[] input = br.readLine().split(" ");
-                char operation = input[0].charAt(0);
-                int n = Integer.parseInt(input[1]);
-                
-                if (operation == 'I') {
-                    queue.put(n, queue.getOrDefault(n, 0) + 1);
-                } else if (!queue.isEmpty()) {
-                    int key = n == 1 ? queue.lastKey() : queue.firstKey();
-                    if (queue.put(key, queue.get(key) - 1) == 1) {
-                        queue.remove(key);
+            TreeMap<Integer, Integer> treeMap = new TreeMap<>(Comparator.reverseOrder());
+            while (k-- > 0) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                String cmd = st.nextToken();
+                int num = Integer.parseInt(st.nextToken());
+                if (cmd.equals("I")) {
+                    treeMap.put(num, treeMap.getOrDefault(num, 0) + 1);
+                } else if (!treeMap.isEmpty()) {
+                    int key = num == 1 ? treeMap.firstKey() : treeMap.lastKey();
+                    if (treeMap.put(key, treeMap.get(key) - 1) == 1) {
+                        treeMap.remove(key);
                     }
                 }
             }
-            
-            if (queue.isEmpty()) {
-                bw.write("EMPTY\n");
-            } else {
-                bw.write(queue.lastKey() + " " + queue.firstKey() + "\n");
-            }
+            System.out.println(treeMap.isEmpty()? "EMPTY" : treeMap.firstKey() +" " + treeMap.lastKey());
         }
-        
-        bw.flush();
-        bw.close();
-        br.close();
     }
 }
