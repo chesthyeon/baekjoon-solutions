807a2b302b6648e2496d566172c11ef986173baa
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2024-10-01 19:15:15 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,43 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static ArrayList<Integer>[] graph;
-    static boolean[] visited;
-    static int count = 0;
-
+    static boolean visited[];
+    static List<List<Integer>> graph = new ArrayList();
+    static int res = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine()); // 컴퓨터의 수
         int m = Integer.parseInt(br.readLine()); // 연결된 컴퓨터 쌍의 수
-
-        graph = new ArrayList[n+1];
-        visited = new boolean[n+1];
-
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
+        visited = new boolean[n + 1];
+        while (n-- >= 0) {
+            graph.add(new ArrayList<>());
         }
-
-        for (int i = 0; i < m; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            graph[a].add(b);
-            graph[b].add(a);
+        while (m-- > 0) {
+            String[] input = br.readLine().split(" ");
+            int num1 = Integer.parseInt(input[0]);
+            int num2 = Integer.parseInt(input[1]);
+            graph.get(num1).add(num2);
+            graph.get(num2).add(num1);
         }
-
         dfs(1);
-
-        System.out.println(count - 1); // 1번 컴퓨터 제외
+        System.out.println(res - 1);
     }
-
-    static void dfs(int v) {
-        visited[v] = true;
-        count++;
-
-        for (int next : graph[v]) {
+    public static void dfs(int x){
+        visited[x] = true;
+        for (int next : graph.get(x)) {
             if (!visited[next]) {
                 dfs(next);
             }
         }
+        res++;
     }
 }
