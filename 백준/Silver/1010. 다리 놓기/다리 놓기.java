a0f453ea6d9a2a90ea800758934c5b40b3cf35eb
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2024-10-15 15:43:03 +0900

    [Silver V] Title: 다리 놓기, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -6,29 +6,30 @@ public class Main {
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
         int T = Integer.parseInt(br.readLine());
         
-        // 미리 조합 값을 계산
-        for (int i = 0; i < 30; i++) {
-            dp[i][i] = 1;
-            dp[i][0] = 1;
-        }
-        
-        for (int i = 2; i < 30; i++) {
-            for (int j = 1; j < 30; j++) {
-                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-            }
-        }
-        
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < T; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
             
-            sb.append(dp[M][N]).append('\n');
+            sb.append(combi(M, N)).append('\n');
         }
         
-        System.out.print(sb);
+        System.out.println(sb);
+    }
+    
+    static int combi(int n, int r) {
+        if (dp[n][r] > 0) {
+            return dp[n][r];
+        }
+        
+        if (n == r || r == 0) {
+            return dp[n][r] = 1;
+        }
+        
+        return dp[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
     }
 }
