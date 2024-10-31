3a19d17a9330b1d09690143962161065be6a7b47
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2024-10-31 09:37:08 +0900

    [Gold V] Title: 톱니바퀴, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -1,79 +1,36 @@
 import java.io.*;
 import java.util.*;
 
-class Gear {
-    int[] teeth; // 톱니바퀴의 상태
-    int west;    // 서쪽 위치 (2번 인덱스)
-    int north;   // 북쪽 위치 (0번 인덱스)
-    int east;    // 동쪽 위치 (6번 인덱스)
-    
-    public Gear(String state) {
-        teeth = new int[8];
-        for(int i = 0; i < 8; i++) {
-            teeth[i] = state.charAt(i) - '0';
-        }
-        west = 6;  // 초기 위치 설정
-        north = 0;
-        east = 2;
-    }
-    
-    // 시계 방향 회전
-    public void clockwise() {
-        west = (west + 7) % 8;
-        north = (north + 7) % 8;
-        east = (east + 7) % 8;
-    }
-    
-    // 반시계 방향 회전
-    public void counterClockwise() {
-        west = (west + 1) % 8;
-        north = (north + 1) % 8;
-        east = (east + 1) % 8;
-    }
-    
-    // 현재 톱니바퀴의 동쪽 극 얻기
-    public int getEastPole() {
-        return teeth[east];
-    }
-    
-    // 현재 톱니바퀴의 서쪽 극 얻기
-    public int getWestPole() {
-        return teeth[west];
-    }
-    
-    // 현재 톱니바퀴의 12시방향 극 얻기
-    public int getNorthPole() {
-        return teeth[north];
-    }
-}
-
 public class Main {
-    static Gear[] gears;
+    static int[][] gears = new int[4][8];
+    static boolean[] visited;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        // 톱니바퀴 4개 초기화
-        gears = new Gear[4];
+        // 톱니바퀴 상태 입력
         for(int i = 0; i < 4; i++) {
-            gears[i] = new Gear(br.readLine());
+            String line = br.readLine();
+            for(int j = 0; j < 8; j++) {
+                gears[i][j] = line.charAt(j) - '0';
+            }
         }
         
         int K = Integer.parseInt(br.readLine());
         
-        // K번 회전
-        for(int i = 0; i < K; i++) {
+        while(K-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int gearNum = Integer.parseInt(st.nextToken()) - 1;  // 0-based index로 변환
+            int gearNum = Integer.parseInt(st.nextToken()) - 1;
             int direction = Integer.parseInt(st.nextToken());
             
+            visited = new boolean[4];
             rotate(gearNum, direction);
         }
         
         // 점수 계산
         int score = 0;
         for(int i = 0; i < 4; i++) {
-            if(gears[i].getNorthPole() == 1) {
+            if(gears[i][0] == 1) {
                 score += (1 << i);
             }
         }
@@ -82,35 +39,43 @@ public class Main {
     }
     
     static void rotate(int gearNum, int direction) {
-        // 각 톱니바퀴의 회전 방향을 저장할 배열
-        int[] rotateDirection = new int[4];
-        rotateDirection[gearNum] = direction;
+        visited[gearNum] = true;
         
-        // 왼쪽 톱니바퀴들 확인
-        for(int i = gearNum - 1; i >= 0; i--) {
-            if(gears[i].getEastPole() != gears[i + 1].getWestPole()) {
-                rotateDirection[i] = -rotateDirection[i + 1];
-            } else {
-                break;
+        // 왼쪽 톱니바퀴 확인
+        if(gearNum > 0 && !visited[gearNum-1]) {
+            if(gears[gearNum][6] != gears[gearNum-1][2]) {
+                rotate(gearNum-1, -direction);
             }
         }
         
-        // 오른쪽 톱니바퀴들 확인
-        for(int i = gearNum + 1; i < 4; i++) {
-            if(gears[i].getWestPole() != gears[i - 1].getEastPole()) {
-                rotateDirection[i] = -rotateDirection[i - 1];
-            } else {
-                break;
+        // 오른쪽 톱니바퀴 확인
+        if(gearNum < 3 && !visited[gearNum+1]) {
+            if(gears[gearNum][2] != gears[gearNum+1][6]) {
+                rotate(gearNum+1, -direction);
             }
         }
         
-        // 모든 톱니바퀴 회전
-        for(int i = 0; i < 4; i++) {
-            if(rotateDirection[i] == 1) {
-                gears[i].clockwise();
-            } else if(rotateDirection[i] == -1) {
-                gears[i].counterClockwise();
-            }
+        // 현재 톱니바퀴 회전
+        if(direction == 1) {
+            rotateClockwise(gearNum);
+        } else {
+            rotateCounterClockwise(gearNum);
         }
+    }
+    
+    static void rotateClockwise(int gearNum) {
+        int temp = gears[gearNum][7];
+        for(int i = 7; i > 0; i--) {
+            gears[gearNum][i] = gears[gearNum][i-1];
+        }
+        gears[gearNum][0] = temp;
+    }
+    
+    static void rotateCounterClockwise(int gearNum) {
+        int temp = gears[gearNum][0];
+        for(int i = 0; i < 7; i++) {
+            gears[gearNum][i] = gears[gearNum][i+1];
+        }
+        gears[gearNum][7] = temp;
     }
 }
