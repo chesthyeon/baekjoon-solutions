0f2c2f44ee756687c2e158bcb34b3723cdd8b0da
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2024-10-31 20:25:43 +0900

    [level 2] Title: 타겟 넘버, Time: 5.80 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,26 +1,23 @@
 import java.util.*;
 
 class Solution {
+    int answer;
+    int len;
     public int solution(int[] numbers, int target) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(0);
-        
-        for (int number : numbers) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int current = queue.poll();
-                queue.offer(current + number);
-                queue.offer(current - number);
+        answer = 0;
+        len = numbers.length;
+        dfs(numbers, target, 0, 0);
+        return answer;
+    }
+    public void dfs(int[] numbers, int target, int cnt, int sum){
+        if (cnt == len) {
+            if (sum == target) {
+                answer++;
             }
+            return;
         }
+        dfs(numbers, target, cnt + 1, sum + numbers[cnt]);
+        dfs(numbers, target, cnt + 1, sum - numbers[cnt]);
         
-        int count = 0;
-        while (!queue.isEmpty()) {
-            if (queue.poll() == target) {
-                count++;
-            }
-        }
-        
-        return count;
     }
 }
