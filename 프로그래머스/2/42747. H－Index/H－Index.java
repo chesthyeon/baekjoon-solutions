4e7036a0cd9c19488d5d2e0a9968aacaa75a8b53
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2024-10-31 22:28:04 +0900

    [level 2] Title: H-Index, Time: 0.87 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -4,10 +4,10 @@ class Solution {
     public int solution(int[] citations) {
         int answer = 0;
         Arrays.sort(citations);
-        
-        for (int i = 0; i < citations.length; i++){
-            int smaller = Math.min(citations[i],citations.length - i);
-            answer = Math.max(answer, smaller);
+        for (int i = 0; i < citations.length; i++) {
+            if (citations[i] >= citations.length - i) {
+                return citations.length - i;
+            }
         }
         return answer;
     }
