4f49569dd7849019032023f713d8694a8d143f0d
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-04-03 14:11:16 +0900

    [level 2] Title: 방문 길이, Time: 0.58 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,25 +1,38 @@
+import java.util.*;
+import java.util.stream.*;
+
 class Solution {
-        public int solution(String dirs) {
-            int answer = 0;
-            int x = 0;
-            int y = 0;
-            boolean[][][][] visited = new boolean[11][11][11][11];
-            for (char c : dirs.toCharArray()){
-                int nx = x;
-                int ny = y;
-                if (c == 'U') nx += 1;
-                else if (c == 'D') nx -= 1;
-                else if (c == 'R') ny += 1;
-                else if (c == 'L') ny -= 1;
-                if (nx < -5 || nx > 5 || ny < -5 || ny > 5) continue;
-                if (!visited[x + 5][y + 5][nx + 5][ny + 5]){
-                    visited[x + 5][y + 5][nx + 5][ny + 5] = true;
-                    visited[nx + 5][ny + 5][x + 5][y + 5] = true;
-                    answer += 1;
-                }
-                x = nx;
-                y = ny;
-            }
-            return answer;
-        }
+    static boolean isValid(int nx, int ny) {
+        return 0 <= nx && nx < 11 && 0 <= ny && ny < 11;
     }
+    static HashMap<Character, int[]> location = new HashMap<>();
+    static void initLocation() {
+        location.put('U', new int[] {-1, 0});
+        location.put('D', new int[] {1, 0});
+        location.put('R', new int[] {0, 1});
+        location.put('L', new int[] {0, -1});
+    }
+    public int solution(String dirs) {
+        initLocation();
+        int ans = 0;
+        int x = 5;
+        int y = 5;
+        boolean[][][][] visited = new boolean[11][11][11][11];
+        for (char c : dirs.toCharArray()) {
+            int nx = x + location.get(c)[0];
+            int ny = y + location.get(c)[1];
+            if (!isValid(nx, ny)) {
+                continue;
+            }
+            if (!visited[x][y][nx][ny]) {
+                visited[x][y][nx][ny] = true;
+                visited[nx][ny][x][y] = true;
+                ans++;
+            }
+
+            x = nx;
+            y = ny;
+        }
+        return ans;
+    }
+}
