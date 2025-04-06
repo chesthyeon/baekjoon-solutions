1c015967704f91bf51099e0e87eea8653720c714
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-04-06 10:40:35 +0900

    [level 2] Title: 큰 수 만들기, Time: 90.48 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,22 +1,31 @@
+import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     public String solution(String number, int k) {
-        StringBuilder result = new StringBuilder();
-        int length = number.length() - k; // 최종적으로 만들어야 하는 길이
-        int start = 0;
-        while (length > 0) {
-            int end = number.length() - length + 1;
-            char max = '0';
-            int maxIdx = start;
-            for (int i = start; i < end; i++){
-                if (number.charAt(i) > max){
-                    max = number.charAt(i);
-                    maxIdx = i;
-                }
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < number.length(); i++) {
+            char current = number.charAt(i);
+            
+            // 스택이 비어있지 않고, 스택의 top이 현재 숫자보다 작고, 아직 제거할 숫자가 남아있을 때
+            while (!stack.isEmpty() && stack.peek() < current && k > 0) {
+                stack.pop(); // 작은 숫자 제거
+                k--;
             }
-            result.append(max);
-            start = maxIdx + 1;
-            length--;
+            
+            stack.push(current);
         }
-        return result.toString();
+        
+        // k가 남아있는 경우 (모든 숫자를 순회했는데도 k개를 제거하지 못한 경우)
+        while (k > 0) {
+            stack.pop();
+            k--;
+        }
+        
+        // 스택을 문자열로 변환 (스트림과 람다 사용)
+        return stack.stream()
+                    .map(String::valueOf)
+                    .collect(Collectors.joining());
     }
 }
