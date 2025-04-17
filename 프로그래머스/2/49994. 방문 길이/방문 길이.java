87a225de72af89f4b3c3f917e04fd4442b863e91
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-04-17 14:35:19 +0900

    [level 2] Title: 방문 길이, Time: 0.66 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,35 +1,35 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
-    static boolean isValid(int nx, int ny) {
-        return 0 <= nx && nx < 11 && 0 <= ny && ny < 11;
-    }
-    static HashMap<Character, int[]> location = new HashMap<>();
-    static void initLocation() {
-        location.put('U', new int[] {-1, 0});
-        location.put('D', new int[] {1, 0});
-        location.put('R', new int[] {0, 1});
-        location.put('L', new int[] {0, -1});
-    }
     public int solution(String dirs) {
-        initLocation();
-        int ans = 0;
-        int x = 5;
-        int y = 5;
+        Map<Character, int[]> location = new HashMap();
+        location.put('U',new int[]{-1, 0});
+        location.put('D',new int[]{1, 0});
+        location.put('L',new int[]{0, -1});
+        location.put('R',new int[]{0, 1});
+        
+        int x = 5, y = 5;
         boolean[][][][] visited = new boolean[11][11][11][11];
-        for (char c : dirs.toCharArray()) {
+        int ans = 0;
+        
+        for(char c : dirs.toCharArray()){
             int nx = x + location.get(c)[0];
             int ny = y + location.get(c)[1];
-            if (!isValid(nx, ny)) {
+            
+            if(0 > nx || nx > 10 || 0 > ny || ny > 10){
+             continue;
+            }
+            
+            if(visited[x][y][nx][ny]){
+                x = nx;
+                y = ny;
                 continue;
             }
-            if (!visited[x][y][nx][ny]) {
-                visited[x][y][nx][ny] = true;
-                visited[nx][ny][x][y] = true;
-                ans++;
-            }
-
+            
+            visited[x][y][nx][ny] = true;
+            visited[nx][ny][x][y] = true;
+            ans++;
+            
             x = nx;
             y = ny;
         }
