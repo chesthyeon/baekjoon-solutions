6443c8f69e47ed7256cfde13f0399452550f6374
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-04-18 16:48:34 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 21.90 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 87.3 MB, 시간: 19.67 ms
+메모리: 96.6 MB, 시간: 21.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 03일 22:37:05
+2025년 04월 18일 16:48:31
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,33 +2,35 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        // 다리 위 트럭을 관리하는 큐 [트럭무게, 진입시간]
-        Queue<int[]> bridge = new LinkedList<>();
+        int time = 0;
+        int curWeight = 0;
         
-        int time = 0;        // 현재 시간
-        int currentWeight = 0; // 다리 위 트럭 총 무게
-        int truckIndex = 0;  // 대기 중인 트럭 인덱스
+        Queue<Integer> bridge = new LinkedList<>();
+        // 다리 초기화
+        for (int i = 0; i < bridge_length; i++) {
+            bridge.offer(0);
+        }
         
-        // 모든 트럭이 다리를 건널 때까지 반복
-        while (truckIndex < truck_weights.length || !bridge.isEmpty()) {
-            time++; // 시간 증가
+        int idx = 0; // 현재 처리할 트럭 인덱스
+        
+        // 모든 트럭이 다리를 건널 때까지
+        while (idx < truck_weights.length) {
+            // 다리의 맨 앞 요소 제거하고 무게 갱신
+            curWeight -= bridge.poll();
+            time++;
             
-            // 다리를 빠져나갈 트럭이 있는지 확인
-            if (!bridge.isEmpty() && time - bridge.peek()[1] >= bridge_length) {
-                int[] exitTruck = bridge.poll();
-                currentWeight -= exitTruck[0];
-            }
-            
-            // 새 트럭이 다리에 진입할 수 있는지 확인
-            if (truckIndex < truck_weights.length && 
-                currentWeight + truck_weights[truckIndex] <= weight) {
-                // 트럭을 다리에 추가 [트럭무게, 진입시간]
-                bridge.offer(new int[]{truck_weights[truckIndex], time});
-                currentWeight += truck_weights[truckIndex];
-                truckIndex++;
+            // 현재 트럭이 다리에 올라갈 수 있는지 확인
+            if (curWeight + truck_weights[idx] <= weight) {
+                bridge.offer(truck_weights[idx]);
+                curWeight += truck_weights[idx];
+                idx++; // 다음 트럭으로 이동
+            } else {
+                bridge.offer(0); // 트럭이 올라갈 수 없으면 빈 공간 추가
             }
         }
         
-        return time;
+        // 마지막 트럭이 다리를 완전히 건너는 시간 추가
+        // 마지막 트럭이 들어간 직후 다리 길이만큼의 시간이 더 필요
+        return time + bridge_length;
     }
 }
