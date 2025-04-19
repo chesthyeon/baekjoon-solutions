754b2b97967788fd9822a96eba6bb6f399561343
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-04-19 16:21:15 +0900

    [level 3] Title: 섬 연결하기, Time: 0.81 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,61 +1,43 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
-    // 각 노드의 부모를 저장하는 배열
-    private int[] parent;
-    
-    // 노드가 속한 집합의 루트 노드 찾기
-    private int find(int node) {
-        if (parent[node] == node) {
-            return node;
-        }
-        // 경로 압축: 재귀적으로 루트를 찾으며 parent 값을 루트로 업데이트
-        return parent[node] = find(parent[node]);
-    }
-    
-    // 두 노드가 속한 집합 합치기
-    private void union(int node1, int node2) {
-        int root1 = find(node1);
-        int root2 = find(node2);
-        
-        // 이미 같은 집합이면 수행하지 않음
-        if (root1 != root2) {
-            parent[root2] = root1;
-        }
-    }
-    
     public int solution(int n, int[][] costs) {
-        // 간선을 비용 기준으로 오름차순 정렬
-        Arrays.sort(costs, (a, b) -> a[2] - b[2]);
-        
-        // 부모 배열 초기화: 처음에는 자기 자신이 부모
-        parent = new int[n];
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
+        int answer = 0;
+        Arrays.sort(costs, (a, b) -> (a[2] - b[2]));
+
+        int[] parents = new int[n];
+        for (int i = 0; i < parents.length; i++) {
+            parents[i] = i;
         }
-        
-        int totalCost = 0;
-        int connectedBridges = 0;
-        
-        // 모든 간선을 비용이 적은 순서대로 확인
+
+        int bridge = 0;
         for (int[] cost : costs) {
             int island1 = cost[0];
             int island2 = cost[1];
-            int bridgeCost = cost[2];
-            
-            // 두 섬이 아직 연결되지 않았다면
-            if (find(island1) != find(island2)) {
-                union(island1, island2);  // 두 섬 연결
-                totalCost += bridgeCost;  // 비용 추가
-                connectedBridges++;       // 연결된 다리 수 증가
-                
-                // MST 완성: n개의 섬을 연결하는데 필요한 다리는 n-1개
-                if (connectedBridges == n - 1) {
-                    break;
-                }
+            int costNum = cost[2];
+
+            if (find(island1, parents) == find(island2, parents)) {
+                continue;
+            }
+
+            union(island1, island2, parents);
+            bridge += 1;
+            answer += costNum;
+
+            if (bridge >= n - 1) {
+                return answer;
             }
         }
-        
-        return totalCost;
+        return answer;
+    }
+
+    int find(int island, int[] parents) {
+        if (parents[island] == island) {
+            return island;
+        }
+        return parents[island] = find(parents[island], parents);
+    }
+    void union(int island1, int island2, int[] parents){
+        parents[find(island2, parents)] = find(island1, parents);
     }
 }
