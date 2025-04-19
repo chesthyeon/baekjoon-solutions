5fd11fd14b5008bd5a784423ea3e9588120a5302
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-04-19 15:52:34 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,30 +1,24 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        Set<String> usedWords = new HashSet<>();
-        
-        // 첫 단어 처리
-        usedWords.add(words[0]);
-        
+        HashSet<String> wordSets = new HashSet<>();
+        wordSets.add(words[0]);
+        char pre = words[0].charAt(words[0].length() - 1);
+        int[] ans = new int[2];
         for (int i = 1; i < words.length; i++) {
-            String prevWord = words[i-1];
-            String currentWord = words[i];
-            
-            char lastCharOfPrev = prevWord.charAt(prevWord.length() - 1);
-            char firstCharOfCurrent = currentWord.charAt(0);
-            
-            // 탈락 조건 확인: 이미 사용된 단어이거나 끝말잇기 규칙을 어긴 경우
-            if (usedWords.contains(currentWord) || lastCharOfPrev != firstCharOfCurrent) {
-                // 탈락자 번호: (i % n) + 1, 탈락자 차례: (i / n) + 1
-                return new int[] {(i % n) + 1, (i / n) + 1};
+            String word = words[i];
+            char cur = word.charAt(0);
+            if (wordSets.contains(word) || pre != cur) {
+                ans[0] = i % n + 1;
+                ans[1] = i / n + 1;
+                return ans;
+            } else {
+                wordSets.add(word);
+                pre = word.charAt(word.length() - 1);
             }
-            
-            usedWords.add(currentWord);
         }
-        
-        // 탈락자가 없는 경우
-        return new int[] {0, 0};
+        return ans;
     }
 }
