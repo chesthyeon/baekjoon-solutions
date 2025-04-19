f48309ccba617dc4829b24c9e2f9b7463a48eead
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-04-20 04:29:34 +0900

    [level 2] Title: 땅따먹기, Time: 22.21 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -2,30 +2,15 @@ import java.util.*;
 
 class Solution {
     int solution(int[][] land) {
-        int n = land.length;
-        
-        // 첫 번째 행은 초기값으로 사용
-        // 두 번째 행부터 시작
-        for (int i = 1; i < n; i++) {
-            // 현재 행의 각 열을 순회
-            for (int j = 0; j < 4; j++) {
-                // 이전 행에서 현재 열을 제외한 최댓값을 찾아 더함
-                int max = 0;
-                for (int k = 0; k < 4; k++) {
-                    if (k != j && land[i-1][k] > max) {
-                        max = land[i-1][k];
-                    }
-                }
-                land[i][j] += max;
-            }
+        for (int i = 1; i < land.length; i++) {
+            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][2]));
         }
         
-        // 마지막 행에서 최댓값 반환
-        int maxScore = 0;
-        for (int j = 0; j < 4; j++) {
-            maxScore = Math.max(maxScore, land[n-1][j]);
-        }
-        
-        return maxScore;
+        int lastRow = land.length - 1;
+        return Math.max(Math.max(land[lastRow][0], land[lastRow][1]), 
+                        Math.max(land[lastRow][2], land[lastRow][3]));
     }
 }
