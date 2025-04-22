d43f9cf00b4c0b270f44e9a759df62d24967355b
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-04-23 02:18:25 +0900

    [level 3] Title: 여행경로, Time: 0.83 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,53 +1,49 @@
 import java.util.*;
 
 class Solution {
-    TreeMap<String, PriorityQueue<String>> graph;
-    List<String> route;
-    int totalTickets;
-    
+    boolean[] used;
+    String[] answer;
+    boolean finished;
+
     public String[] solution(String[][] tickets) {
-        // TreeMap으로 변경: 출발지도 자동 정렬됨
-        graph = new TreeMap<>();
-        totalTickets = tickets.length;
-        
-        for (String[] ticket : tickets) {
-            graph.putIfAbsent(ticket[0], new PriorityQueue<>());
-            graph.get(ticket[0]).add(ticket[1]);
-        }
-        
-        route = new ArrayList<>();
-        route.add("ICN");
-        dfs("ICN");
-        
-        return route.toArray(new String[0]);
-    }
-    
-    private boolean dfs(String current) {
-        if (route.size() == totalTickets + 1) {
-            return true;
-        }
-        
-        // 현재 공항에서 출발하는 항공편이 없는 경우
-        if (!graph.containsKey(current) || graph.get(current).isEmpty()) {
-            return false;
-        }
-        
-        // 현재 공항에서 갈 수 있는 공항들을 탐색
-        PriorityQueue<String> destinations = graph.get(current);
-        String[] temp = destinations.toArray(new String[0]);
-        
-        for (String next : temp) {
-            destinations.remove(next);
-            route.add(next);
-            
-            if (dfs(next)) {
-                return true;
+        // 알파벳 순서로 티켓 정렬
+        Arrays.sort(tickets, (a, b) -> {
+            if (a[0].equals(b[0])) {
+                return a[1].compareTo(b[1]);
             }
-            
-            route.remove(route.size() - 1);
-            destinations.add(next);
+            return a[0].compareTo(b[0]);
+        });
+
+        used = new boolean[tickets.length];
+        answer = new String[tickets.length + 1];
+        finished = false;
+
+        // 항상 "ICN"에서 출발
+        answer[0] = "ICN";
+        dfs(0, "ICN", tickets);
+
+        return answer;
+    }
+
+    private void dfs(int depth, String current, String[][] tickets) {
+        if (depth == tickets.length) {
+            finished = true;
+            return;
         }
-        
-        return false;
+
+        for (int i = 0; i < tickets.length; i++) {
+            if (!used[i] && tickets[i][0].equals(current)) {
+                used[i] = true;
+                answer[depth + 1] = tickets[i][1];
+
+                dfs(depth + 1, tickets[i][1], tickets);
+
+                if (!finished) {
+                    used[i] = false;
+                } else {
+                    break;
+                }
+            }
+        }
     }
 }
