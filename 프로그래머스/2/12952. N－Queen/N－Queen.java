13b054890b076bd38634989c6aa18d304a81c2fe
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-04-23 20:45:10 +0900

    [level 2] Title: N-Queen, Time: 64.50 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,45 +1,30 @@
-public class Solution {
+class Solution {
+    public int solution(int n) {
+        boolean[] w = new boolean[n];
+        boolean[] dUp = new boolean[n * 2];
+        boolean[] dDown = new boolean[n * 2];
+        return dfs(n, 0, w, dUp, dDown);
+    }
 
-    private static int N;
-    private static boolean[] width;
-    private static boolean[] diagonal1;
-    private static boolean[] diagonal2;
-
-    // ❶ 퀸이 서로 공격할 수 없는 위치에 놓이는 경우의 수를 구하는 함수
-    private static int getAns(int y) {
-        int ans = 0;
-        // ❷ 모든 행에 대해서 퀸의 위치가 결장되었을 경우
-        if (y == N) {
-            // ❸ 해결 가능한 경우의 수를 1 증가시킴
-            ans++;
+    int dfs(int n, int row,  boolean[] w, boolean[] dUp, boolean[] dDown){
+        if (row == n) {
+            return 1;
         }
-        else {
-            // ❹ 현재 행에서 퀸이 놓일 수 있는 모든 위치를 시도
-            for (int i = 0; i < N; i++) {
-                // ❺ 해당 위치에 이미 퀸이 있는 경우, 대각선상에 퀸이 있는 경우 스킵
-                if (width[i] || diagonal1[i + y] || diagonal2[i - y + N])
-                    continue;
 
-                // ❻ 해당 위치에 퀸을 놓음
-                width[i] = diagonal1[i + y] = diagonal2[i - y + N] = true;
-                // ❼ 다음 행으로 이동하여 재귀적으로 해결 가능한 경우의 수 찾기
-                ans += getAns(y + 1);
-                // ❽ 해당 위치에 놓인 퀸을 제거함
-                width[i] = diagonal1[i + y] = diagonal2[i - y + N] = false;
+        int cnt = 0;
+        for (int col = 0; col < n; col++) {
+            if(!w[col] && !dUp[row+col] && !dDown[col-row+n]){
+                w[col] = true;
+                dUp[row+col] = true;
+                dDown[col - row + n] = true;
+
+                cnt += dfs(n, row + 1, w, dUp, dDown);
+
+                w[col] = false;
+                dUp[row+col] = false;
+                dDown[col - row + n] = false;
             }
         }
-
-        return ans;
+        return cnt;
     }
-
-    public int solution(int n) {
-        N = n;
-        width = new boolean[n];
-        diagonal1 = new boolean[n * 2];
-        diagonal2 = new boolean[n * 2];
-
-        int answer = getAns(0);
-        return answer;
-    }
-
 }
