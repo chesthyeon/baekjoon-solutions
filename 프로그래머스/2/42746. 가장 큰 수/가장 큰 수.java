4356d273449e214ae9c021b5ebaeae79bd25ac41
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-04-24 03:07:40 +0900

    [level 2] Title: 가장 큰 수, Time: 192.82 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,28 +1,25 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public String solution(int[] numbers) {
-        // 커스텀 비교자를 가진 우선순위 큐 생성
-        PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> {
-            String order1 = b + a; // 순서 주의: b + a로 해야 내림차순
-            String order2 = a + b;
-            return order1.compareTo(order2);
-        });
-        
-        // 숫자들을 문자열로 변환하여 우선순위 큐에 추가
-        for (int number : numbers) {
-            pq.offer(String.valueOf(number));
+        // 숫자들을 문자열 배열로 변환
+        String[] strNumbers = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            strNumbers[i] = String.valueOf(numbers[i]);
         }
         
-        // 모든 수가 0인 경우 처리
-        if (pq.peek().equals("0")) {
+        // 정렬: a+b와 b+a를 비교하여 더 큰 숫자 조합이 앞으로 오도록
+        Arrays.sort(strNumbers, (a, b) -> (b + a).compareTo(a + b));
+        
+        // 정렬된 결과가 0으로 시작한다면 모든 숫자가 0이라는 의미
+        if (strNumbers[0].equals("0")) {
             return "0";
         }
         
-        // 우선순위 큐에서 순서대로 추출하여 결과 문자열 생성
+        // 정렬된 결과를 하나의 문자열로 연결
         StringBuilder answer = new StringBuilder();
-        while (!pq.isEmpty()) {
-            answer.append(pq.poll());
+        for (String str : strNumbers) {
+            answer.append(str);
         }
         
         return answer.toString();
