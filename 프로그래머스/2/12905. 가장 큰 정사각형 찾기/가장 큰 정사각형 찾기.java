80f1e3e8627d97018c044372d46b343034987c67
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-04-24 20:01:50 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 14.83 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -1,36 +1,25 @@
-public class Solution {
+class Solution
+{
+    public int solution(int [][]board)
+    {
+        int h = board.length;
+        int w = board[0].length;
+        int ans = 0;
 
-    public int solution(int [][]board) {
-        // ❶ 주어진 2차원 보드의 행과 열의 개수를 변수에 저장합니다.
-        int row = board.length;
-        int col = board[0].length;
-
-        // ❷ 각 행과 열을 순회하며 최적의 정사각형을 찾습니다.
-        for (int i = 1; i < row; i++) {
-            for (int j = 1; j < col; j++) {
-                // ❸ 현재 위치의 값이 1인 경우를 확인합니다.
-                if (board[i][j] == 1) {
-                    // ❹ 현재 위치에서 위, 왼쪽, 대각선 왼쪽 위의 값들을 가져옵니다.
-                    int up = board[i - 1][j];
-                    int left = board[i][j - 1];
-                    int upLeft = board[i - 1][j - 1];
-                    // ❺ 현재 위치의 값을 이전 위치들의 값들 중
-                    // 가장 작은 값에 1을 더한 값으로 업데이트 합니다.
-                    board[i][j] += Math.min(up, Math.min(upLeft, left));
-                }
+        for (int i = 0; i < h; i++) {
+            if (board[i][0] == 1) ans = 1;
+        }
+        for (int j = 0; j < w; j++) {
+            if (board[0][j] == 1) ans = 1;
+        }
+        
+        for (int i = 1; i < h; i++) {
+            for (int j = 1; j < w; j++) {
+                if (board[i][j] == 0) continue;
+                board[i][j] = Math.min(board[i - 1][j - 1], Math.min(board[i - 1][j], board[i][j - 1])) + 1;
+                ans = Math.max(board[i][j], ans);
             }
         }
-
-        int answer = 0;
-        // ❻ 보드에서 가장 큰 값(최대 정사각형의 한 변의 길이)을 찾습니다.
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                answer = Math.max(answer, board[i][j]);
-            }
-        }
-
-        // ❼ 최대 정사각형의 넓이를 반환합니다.
-        return answer * answer;
+        return ans*ans;
     }
-
 }
