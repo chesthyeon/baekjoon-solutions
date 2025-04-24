c61d18435aa7dc09cb3786d7f19cfa99eda34a9e
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-04-24 19:41:58 +0900

    [level 3] Title: 정수 삼각형, Time: 13.52 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,14 +1,13 @@
-import java.util.*;
+public class Solution {
 
-class Solution {
     public int solution(int[][] triangle) {
-        for (int i = 1; i < triangle.length; i++) {
-            triangle[i][0] += triangle[i - 1][0];
-            triangle[i][i] += triangle[i - 1][i - 1];
-            for (int j = 1; j < i; j++) {
-                triangle[i][j] += Math.max(triangle[i - 1][j], triangle[i - 1][j - 1]);
+        for (int i = triangle.length - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
             }
         }
-        return Arrays.stream(triangle[triangle.length - 1]).max().getAsInt();
+
+        return triangle[0][0]; // 꼭대기에서의 최댓값 반환
     }
+
 }
