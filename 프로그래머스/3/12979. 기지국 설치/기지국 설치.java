258aa9867de241f12c3cd15f1a24036cc171ba05
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-04-25 01:08:52 +0900

    [level 3] Title: 기지국 설치, Time: 1.12 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,18 +1,25 @@
 class Solution {
     public int solution(int n, int[] stations, int w) {
-        int location = 1;
-        int idx = 0;
-        int cnt = 0;
-
-        while (location <= n) {
-            if (idx < stations.length && location >=  stations[idx] - w) {
-                location = stations[idx] + w + 1;
-                idx++;
-            } else {
-                location += 2 * w + 1;
-                cnt++;
-            }
+    int answer = 0;
+    int position = 1; // 현재 검사 위치
+    int idx = 0; // 기지국 배열 인덱스
+    
+    while (position <= n) {
+        // 현재 위치가 기존 기지국 범위 안에 있는 경우
+        if (idx < stations.length && position >= stations[idx] - w) {
+            // 해당 기지국의 커버 범위를 벗어난 다음 위치로 이동
+            position = stations[idx] + w + 1;
+            idx++;
+        } 
+        // 현재 위치가 기존 기지국 범위 밖인 경우
+        else {
+            // 새 기지국을 설치하고 카운트 증가
+            answer++;
+            // 새 기지국의 커버 범위 다음 위치로 이동 (2*w+1은 한 기지국이 커버하는 범위)
+            position += 2 * w + 1;
         }
-        return cnt;
     }
+    
+    return answer;
+}
 }
