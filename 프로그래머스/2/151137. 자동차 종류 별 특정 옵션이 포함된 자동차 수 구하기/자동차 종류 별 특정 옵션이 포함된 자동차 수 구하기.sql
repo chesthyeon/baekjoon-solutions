bd592ce0425da83706b2bf82276f5601ec57d8d5
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-07-28 04:07:38 +0900

    [level 2] Title: 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/151137. 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기/자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기.sql
+++ 프로그래머스/2/151137. 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기/자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기.sql
@@ -1,10 +1,42 @@
+
+-- 해답: 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기
 SELECT 
     CAR_TYPE,
-    COUNT(*) AS CARS_COUNT
+    COUNT(*) AS CARS
 FROM 
     CAR_RENTAL_COMPANY_CAR
 WHERE 
-    OPTIONS LIKE '%시트%'
+    OPTIONS LIKE '%통풍시트%' 
+    OR OPTIONS LIKE '%열선시트%' 
+    OR OPTIONS LIKE '%가죽시트%'
+GROUP BY 
+    CAR_TYPE
+ORDER BY 
+    CAR_TYPE;
+
+-- 방법 2: REGEXP 사용 (MySQL)
+SELECT 
+    CAR_TYPE,
+    COUNT(*) AS CARS
+FROM 
+    CAR_RENTAL_COMPANY_CAR
+WHERE 
+    OPTIONS REGEXP '통풍시트|열선시트|가죽시트'
+GROUP BY 
+    CAR_TYPE
+ORDER BY 
+    CAR_TYPE;
+
+-- 방법 3: FIND_IN_SET 사용 (콤마로 구분된 경우)
+SELECT 
+    CAR_TYPE,
+    COUNT(*) AS CARS
+FROM 
+    CAR_RENTAL_COMPANY_CAR
+WHERE 
+    FIND_IN_SET('통풍시트', REPLACE(OPTIONS, ', ', ',')) > 0
+    OR FIND_IN_SET('열선시트', REPLACE(OPTIONS, ', ', ',')) > 0
+    OR FIND_IN_SET('가죽시트', REPLACE(OPTIONS, ', ', ',')) > 0
 GROUP BY 
     CAR_TYPE
 ORDER BY 
