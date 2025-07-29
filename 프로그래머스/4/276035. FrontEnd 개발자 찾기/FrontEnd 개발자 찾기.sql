370bd863461d95ae620b611b87beca47577d83c6
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-07-29 18:47:51 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,17 +1,5 @@
-WITH Front_end_skill_codes AS(
-    SELECT CODE
-    FROM SKILLCODES
-    WHERE CATEGORY = 'Front End'
-)
-SELECT 
-    D.ID,
-    D.EMAIL,
-    D.FIRST_NAME,
-    D.LAST_NAME
-FROM 
-    DEVELOPERS D
-JOIN 
-    Front_end_skill_codes FNSC ON D.SKILL_CODE & FNSC.CODE
-GROUP BY d.id, d.email, d.first_name, d.last_name
-ORDER BY 
-    D.ID ASC;
+SELECT DISTINCT D.ID, D.EMAIL, D.FIRST_NAME, D.LAST_NAME
+FROM DEVELOPERS D
+JOIN SKILLCODES S ON D.SKILL_CODE & S.CODE
+WHERE S.CATEGORY = 'Front End'
+ORDER BY D.ID ASC;
