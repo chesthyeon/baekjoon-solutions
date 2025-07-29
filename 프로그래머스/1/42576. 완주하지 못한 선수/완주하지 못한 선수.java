c56589c90743b74c30df9271bec74a011a50a3c2
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-07-30 01:02:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 90.90 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,22 +1,28 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> nameCnt = new HashMap();
-
-        for (String p : participant) {
-            nameCnt.put(p, nameCnt.getOrDefault(p, 0) + 1);
+        // 1. 참가자들의 이름과 개수를 저장할 HashMap
+        HashMap<String, Integer> participantMap = new HashMap<>();
+        
+        // 2. 참가자 명단을 HashMap에 저장 (이름별 개수 카운팅)
+        for (String name : participant) {
+            participantMap.put(name, participantMap.getOrDefault(name, 0) + 1);
         }
-
-        for (String c : completion) {
-            int cnt = nameCnt.get(c);
-            if (cnt == 1) {
-                nameCnt.remove(c);
-            } else {
-                nameCnt.put(c, cnt - 1);
+        
+        // 3. 완주자 명단을 순회하면서 HashMap에서 개수 차감
+        for (String name : completion) {
+            participantMap.put(name, participantMap.get(name) - 1);
+        }
+        
+        // 4. 개수가 1인 사람이 완주하지 못한 선수
+        for (String name : participantMap.keySet()) {
+            if (participantMap.get(name) == 1) {
+                return name;
             }
         }
-        return nameCnt.keySet().iterator().next();
+        
+        // 이론적으로 여기에 도달할 수 없음 (문제 조건상 반드시 1명 존재)
+        return "";
     }
 }
