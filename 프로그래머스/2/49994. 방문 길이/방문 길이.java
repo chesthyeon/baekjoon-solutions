e8b4c3417b61b70e4c91f96ee274aae83ce3534f
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-08-08 00:09:45 +0900

    [level 2] Title: 방문 길이, Time: 27.22 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,38 +1,33 @@
 import java.util.*;
 
-class Solution {
+public class Solution {
     public int solution(String dirs) {
-        Map<Character, int[]> location = new HashMap();
-        location.put('U',new int[]{-1, 0});
-        location.put('D',new int[]{1, 0});
-        location.put('L',new int[]{0, -1});
-        location.put('R',new int[]{0, 1});
+        Set<String> visitedPaths = new HashSet<>();
+        int x = 0, y = 0; // 시작점 (0,0)
         
-        int x = 5, y = 5;
-        boolean[][][][] visited = new boolean[11][11][11][11];
-        int ans = 0;
-        
-        for(char c : dirs.toCharArray()){
-            int nx = x + location.get(c)[0];
-            int ny = y + location.get(c)[1];
+        for (char dir : dirs.toCharArray()) {
+            int nx = x, ny = y;
             
-            if(0 > nx || nx > 10 || 0 > ny || ny > 10){
-             continue;
+            // 방향에 따른 이동
+            switch (dir) {
+                case 'U': ny++; break;
+                case 'D': ny--; break;
+                case 'L': nx--; break;
+                case 'R': nx++; break;
             }
             
-            if(visited[x][y][nx][ny]){
-                x = nx;
-                y = ny;
-                continue;
-            }
+            // 경계 체크
+            if (nx < -5 || nx > 5 || ny < -5 || ny > 5) continue;
             
-            visited[x][y][nx][ny] = true;
-            visited[nx][ny][x][y] = true;
-            ans++;
+            // 길을 정규화해서 저장 (양방향이므로)
+            String path = Math.min(x, nx) + "," + Math.min(y, ny) + 
+                         "->" + Math.max(x, nx) + "," + Math.max(y, ny);
+            visitedPaths.add(path);
             
             x = nx;
             y = ny;
         }
-        return ans;
+        
+        return visitedPaths.size();
     }
 }
