1cff21e6b94e348dce79d2060f701d5f53e22baf
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-08-09 02:35:46 +0900

    [Silver II] Title: 나무 자르기, Time: 484 ms, Memory: 119408 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        int[] trees = new int[N];
+        int max = 0;
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            trees[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, trees[i]);
+        }
+        
+        System.out.println(binarySearch(trees, M, max));
+    }
+    
+    public static long binarySearch(int[] trees, int M, int max) {
+        long start = 0;
+        long end = max;
+        
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            long sum = 0;
+            
+            for (int tree : trees) {
+                if (tree > mid) {
+                    sum += tree - mid;
+                }
+            }
+            
+            if (sum >= M) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        return end;
+    }
+}
