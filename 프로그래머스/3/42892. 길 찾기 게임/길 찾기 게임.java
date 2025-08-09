f60919b8f683cad05c3197717f69619c5bba9445
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-08-10 01:29:23 +0900

    [level 3] Title: 길 찾기 게임, Time: 31.84 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -1,65 +1,81 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
-public class Solution {
-    
-    private static class Node {
-        int x, y, num;
+class Solution {
+    static class Node {
+        int x, y, id;
         Node left, right;
         
-        public Node(int num, int x, int y) {
-            this.num = num;
+        Node(int x, int y, int id) {
             this.x = x;
             this.y = y;
+            this.id = id;
         }
     }
+    
+    static List<Integer> preorderResult;
+    static List<Integer> postorderResult;
     
     public int[][] solution(int[][] nodeinfo) {
-        // 노드 배열 생성 및 정렬
-        Node[] nodes = new Node[nodeinfo.length];
+        List<Node> nodes = new ArrayList<>();
+        
         for (int i = 0; i < nodeinfo.length; i++) {
-            nodes[i] = new Node(i + 1, nodeinfo[i][0], nodeinfo[i][1]);
+            nodes.add(new Node(nodeinfo[i][0], nodeinfo[i][1], i + 1));
         }
         
-        // y 내림차순, 같으면 x 오름차순 정렬
-        Arrays.sort(nodes, (a, b) -> a.y == b.y ? a.x - b.x : b.y - a.y);
+        nodes.sort((a, b) -> {
+            if (a.y == b.y) {
+                return Integer.compare(a.x, b.x);
+            }
+            return Integer.compare(b.y, a.y);
+        });
         
-        // 트리 구성 (루트는 첫 번째 노드)
-        Node root = nodes[0];
-        for (int i = 1; i < nodes.length; i++) {
-            insertNode(root, nodes[i]);
+        Node root = nodes.get(0);
+        for (int i = 1; i < nodes.size(); i++) {
+            insertNode(root, nodes.get(i));
         }
         
-        // 전위 순회와 후위 순회 결과 저장
-        ArrayList<Integer> preOrder = new ArrayList<>();
-        ArrayList<Integer> postOrder = new ArrayList<>();
-        traversal(root, preOrder, postOrder);
+        preorderResult = new ArrayList<>();
+        postorderResult = new ArrayList<>();
         
-        // 결과 반환
-        return new int[][] {
-            preOrder.stream().mapToInt(i -> i).toArray(),
-            postOrder.stream().mapToInt(i -> i).toArray()
-        };
+        preorder(root);
+        postorder(root);
+        
+        int[][] result = new int[2][];
+        result[0] = preorderResult.stream().mapToInt(Integer::intValue).toArray();
+        result[1] = postorderResult.stream().mapToInt(Integer::intValue).toArray();
+        
+        return result;
     }
     
-    // 노드 삽입 (x 좌표 비교해 왼쪽/오른쪽 결정)
-    private static void insertNode(Node parent, Node child) {
+    void insertNode(Node parent, Node child) {
         if (child.x < parent.x) {
-            if (parent.left == null) parent.left = child;
-            else insertNode(parent.left, child);
+            if (parent.left == null) {
+                parent.left = child;
+            } else {
+                insertNode(parent.left, child);
+            }
         } else {
-            if (parent.right == null) parent.right = child;
-            else insertNode(parent.right, child);
+            if (parent.right == null) {
+                parent.right = child;
+            } else {
+                insertNode(parent.right, child);
+            }
         }
     }
     
-    // 전위 순회와 후위 순회를 한번에 수행
-    private static void traversal(Node node, ArrayList<Integer> pre, ArrayList<Integer> post) {
+    void preorder(Node node) {
         if (node == null) return;
         
-        pre.add(node.num);  // 전위 순회: 현재 노드 먼저 처리
-        traversal(node.left, pre, post);
-        traversal(node.right, pre, post);
-        post.add(node.num); // 후위 순회: 왼쪽, 오른쪽 처리 후 현재 노드
+        preorderResult.add(node.id);
+        preorder(node.left);
+        preorder(node.right);
+    }
+    
+    void postorder(Node node) {
+        if (node == null) return;
+        
+        postorder(node.left);
+        postorder(node.right);
+        postorderResult.add(node.id);
     }
 }
