b29fb57eee6e012c797eb81678a298e19cb5f705
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-09-26 19:29:37 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 34.08 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,36 +1,41 @@
 import java.util.*;
-
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int time = 0;
-        int curWeight = 0;
+        Queue<Integer> waiting = new ArrayDeque<>();
+        Queue<Integer> bridge = new ArrayDeque<>();
         
-        Queue<Integer> bridge = new LinkedList<>();
-        // 다리 초기화
+        // 대기 트럭들을 큐에 넣기
+        for (int truck : truck_weights) {
+            waiting.offer(truck);
+        }
+        
+        // 다리를 0으로 채우기
         for (int i = 0; i < bridge_length; i++) {
             bridge.offer(0);
         }
         
-        int idx = 0; // 현재 처리할 트럭 인덱스
+        int time = 0;
+        int bridgeWeight = 0;
         
-        // 모든 트럭이 다리를 건널 때까지
-        while (idx < truck_weights.length) {
-            // 다리의 맨 앞 요소 제거하고 무게 갱신
-            curWeight -= bridge.poll();
+        while (!waiting.isEmpty()) {
             time++;
             
-            // 현재 트럭이 다리에 올라갈 수 있는지 확인
-            if (curWeight + truck_weights[idx] <= weight) {
-                bridge.offer(truck_weights[idx]);
-                curWeight += truck_weights[idx];
-                idx++; // 다음 트럭으로 이동
+            // 다리에서 트럭 하나 빼기
+            bridgeWeight -= bridge.poll();
+            
+            // 새 트럭이 들어갈 수 있는지 확인
+            if (bridgeWeight + waiting.peek() <= weight) {
+                int truck = waiting.poll();
+                bridge.offer(truck);
+                bridgeWeight += truck;
             } else {
-                bridge.offer(0); // 트럭이 올라갈 수 없으면 빈 공간 추가
+                bridge.offer(0); // 못 들어가면 0 추가
             }
         }
         
         // 마지막 트럭이 다리를 완전히 건너는 시간 추가
-        // 마지막 트럭이 들어간 직후 다리 길이만큼의 시간이 더 필요
-        return time + bridge_length;
+        time += bridge_length;
+        
+        return time;
     }
 }
