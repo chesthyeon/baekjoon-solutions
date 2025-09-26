8c29a3de3b2b62b88b3b5d3592d7d146eee47527
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-09-26 21:04:36 +0900

    [level 1] Title: 신고 결과 받기, Time: 172.30 ms, Memory: 169 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -5,17 +5,12 @@ class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         Map<String, Set<String>> reportMap = new HashMap<>();
         for (String r : report) {
-            String[] rSplit = r.split(" ");
-            if (!reportMap.containsKey(rSplit[1])) {
-                reportMap.put(rSplit[1], new HashSet<>());
-            }
-            reportMap.get(rSplit[1]).add(rSplit[0]);
+            String[] parts = r.split(" ");
+            reportMap.computeIfAbsent(parts[1], key -> new HashSet<>()).add(parts[0]);
         }
 
         Map<String, Integer> id_map = new HashMap<>();
-        for (int i = 0; i < id_list.length; i++) {
-            id_map.put(id_list[i], i);
-        }
+        for (int i = 0; i < id_list.length; i++) id_map.put(id_list[i], i);
 
         int[] ans = new int[id_list.length];
         for (Map.Entry<String, Set<String>> stringSetEntry : reportMap.entrySet()) {
