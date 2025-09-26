04bb202dc1c0c26ec175e655a20a633918bf40cf
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-09-26 22:50:20 +0900

    [level 2] Title: [3차] 압축, Time: 40.41 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -2,35 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String msg) {
-        // 1. 사전 초기화 (A~Z = 1~26)
-        Map<String, Integer> dict = new HashMap<>();
+        HashMap<String, Integer> dictionary = new HashMap<>();
         for (int i = 0; i < 26; i++) {
-            dict.put("" + (char)('A' + i), i + 1);
+            dictionary.put("" + (char)('A' + i), 1 + i);
         }
-        
-        List<Integer> result = new ArrayList<>();
-        int dictNum = 27;  // 다음에 추가할 사전 번호
-        String current = "";  // 현재 처리중인 문자열
-        
-        for (char c : msg.toCharArray()) {
-            String next = current + c;
-            
-            if (dict.containsKey(next)) {
-                // 사전에 있으면 계속 확장
-                current = next;
+
+        StringBuilder pre = new StringBuilder();
+        ArrayList<Integer> ans = new ArrayList<>();
+        int num = 27;
+
+        for (char m : msg.toCharArray()) {
+            StringBuilder cur = new StringBuilder(pre).append(m);
+            if (dictionary.containsKey(cur.toString())) {
+                pre = cur;
             } else {
-                // 사전에 없으면
-                result.add(dict.get(current));     // 현재 문자열 출력
-                dict.put(next, dictNum++);         // 새 문자열 사전에 추가
-                current = "" + c;                  // 현재 문자부터 다시 시작
+                ans.add(dictionary.get(pre.toString()));
+                dictionary.put(cur.toString(), num++);
+                pre = new StringBuilder().append(m);
             }
         }
-        
-        // 마지막 문자열 처리
-        if (!current.isEmpty()) {
-            result.add(dict.get(current));
-        }
-        
-        return result.stream().mapToInt(i -> i).toArray();
+        ans.add(dictionary.get(pre.toString()));
+        return ans.stream().mapToInt(Integer::intValue).toArray();
     }
 }
