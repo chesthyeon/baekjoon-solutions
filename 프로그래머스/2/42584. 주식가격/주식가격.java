fc03d7df78c7272a1c78afbcf69e8dd5f63428c8
Yu Banghyeon <75735367+chesthyeon@users.noreply.github.com>
2025-09-26 17:41:05 +0900

    [level 2] Title: 주식가격, Time: 36.31 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,23 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        Stack<Integer> stack = new Stack();
         stack.push(0);
+        
         int[] ans = new int[prices.length];
-
-        for (int i = 1; i < prices.length; i++) {
-            while(!stack.isEmpty() && prices[i] < prices[stack.peek()]){
-                int temp = stack.pop();
-                ans[temp] = i - temp;
+        
+        for(int i = 1; i < prices.length; i++){
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]){
+               int idx = stack.pop();
+                ans[idx] = i - idx;
             }
             stack.push(i);
         }
-
-        while (!stack.isEmpty()) {
+        
+        while(!stack.isEmpty()){
             int idx = stack.pop();
             ans[idx] = prices.length - idx - 1;
         }
-
+        
         return ans;
     }
 }
